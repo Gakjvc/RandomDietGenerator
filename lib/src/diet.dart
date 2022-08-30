279fd9b6df03ec45fc1c8e9501e3490b523793c3
Gakj <gabrielvilar88@gmail.com>
2022-08-29 21:30:27 -0300

    Change icon[]

---
+++ lib/src/diet.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+
+class Diet extends StatefulWidget {
+}
--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -2,23 +2,30 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'food.dart';
 
+Food frango = Food(name: "Frango", kcal: 125, protein: 20, fats: 5);
+Food ovo = Food(name: "Ovo", kcal: 146, carbs: 0.6, protein: 13.3, fats: 9.5);
 //TODO organize functions
 Widget homePage() {
   return Scaffold(
+    body: ListView(children: [meal()]),
     floatingActionButton: IconButton(
-      onPressed: (() => _calcIntake()),
-      icon: Icon(Icons.ac_unit),
+      onPressed: (() {
+        _calcIntake();
+      }),
+      icon: Icon(Icons.casino_outlined),
     ),
   );
 }
 
 void _calcIntake() {
   double kcal = 2000, dailyProtein, dailyCarbs, dailyFats;
-  Food food1 = Food(name: "Frango", kcal: 125, protein: 20, fats: 5);
-  Food food2 =
-      Food(name: "Ovo", kcal: 146, carbs: 0.6, protein: 13.3, fats: 9.5);
   double a = Random().nextDouble();
   double b = 1 - a;
-  food1.portion = (kcal * a) / food1.kcal;
-  food2.portion = (kcal * b) / food2.kcal;
+  frango.portion = double.parse(((kcal * a) / frango.kcal).toStringAsFixed(1));
+  ovo.portion = double.parse(((kcal * b) / ovo.kcal).toStringAsFixed(1));
+  
+}
+
+Widget meal() {
+  return Text(frango.name + " " + frango.portion.toString());
 }
