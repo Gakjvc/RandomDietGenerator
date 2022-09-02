21e529afbca468e103427785d3e283172745eac1
Gakj <gabrielvilar88@gmail.com>
2022-09-01 22:02:19 -0300

    Function paramaters

--- lib/src/homepage.dart
+++ lib/src/homepage.dart
@@ -10,18 +10,24 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   Food frango = Food(name: "Frango", kcal: 125, protein: 20, fats: 5);
   Food ovo = Food(name: "Ovo", kcal: 146, carbs: 0.6, protein: 13.3, fats: 9.5);
+  List<Food> foods = [];
 //TODO organize functions
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: meal(), floatingActionButton: NewDiet());
+    foods = [frango, ovo];
+    return Scaffold(body: meal(foods), floatingActionButton: NewDiet());
   }
 
-  void _calcIntake(Food food1, Food food2) {
+  void _calcIntake(List<Food> foods) {
     double kcal = 2000, dailyProtein, dailyCarbs, dailyFats;
     double a = Random().nextDouble();
+    // porcao1 +porcao2 + porcao3 = porcaofinal
+    // a + b + c = 1
     double b = 1 - a;
-    food1.portion = double.parse(((kcal * a) / food1.kcal).toStringAsFixed(1));
-    food2.portion = double.parse(((kcal * b) / food2.kcal).toStringAsFixed(1));
+    foods[1].portion =
+        double.parse(((kcal * a) / foods[1].kcal).toStringAsFixed(1));
+    foods[2].portion =
+        double.parse(((kcal * b) / foods[2].kcal).toStringAsFixed(1));
   }
 
   Widget NewDiet() {
@@ -29,16 +35,20 @@ class _HomePageState extends State<HomePage> {
       icon: Icon(Icons.casino_outlined),
       onPressed: (() {
         setState(() {
-          _calcIntake(frango, ovo);
+          _calcIntake(foods);
         });
       }),
     );
   }
 
-  Widget meal() {
-    return ListView(children: [
-      Text(frango.name + " " + frango.portion.toString()),
-      Text(ovo.name + " " + ovo.portion.toString())
-    ]);
+  Widget meal(List<Food> foods) {
+    return ListView(children: [Portion(foods[1]), Portion(foods[2])]);
+  }
+
+  Widget Portion(Food food) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Text(food.name + " " + food.portion.toString()),
+    );
   }
 }
